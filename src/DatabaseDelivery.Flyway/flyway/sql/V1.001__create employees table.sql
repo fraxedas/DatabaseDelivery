73c41f9bc5f9f83b40fef1600a0c5821dabd21d9
Oscar Fraxedas <ofraxedas@microdea.com>
2018-10-25 15:18:48 -0400

    Added a new migrations and a baseline db

--- src/DatabaseDelivery.Flyway/flyway/sql/V1.001__create employees table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[employee](
-	[id] int NOT NULL Identity(1,1),
-	[name] [nvarchar](max) NULL,
-	[email] [nvarchar](max) NULL,
-	[phone] [nvarchar](50) NULL,
-	Primary Key (id)
-)
-
