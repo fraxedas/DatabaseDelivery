f113a1c9cd223f0b313a3322b4fbdbfd94578999
Oscar Fraxedas <ofraxedas@microdea.com>
2018-10-26 15:23:35 -0400

    Updated state based approach

--- src/DatabaseDelivery.State/dbo/Tables/titles.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[titles] (
-    [id]    INT           IDENTITY (1, 1) NOT NULL,
-    [title] VARCHAR (100) NOT NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC)
-);
-
--- src/DatabaseDelivery.State/dbo/Views/employee_positions.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE VIEW employee_positions AS
-SELECT employees.id AS employee_id,
-	name,
-	title
-FROM employees
-	LEFT JOIN titles on employees.title_id = titles.id
