50d18740d4ebdb5deec806da7ac708a457a349f7
Oscar Fraxedas <ofraxedas@microdea.com>
2018-10-29 08:34:04 -0400

    New migration

--- src/DatabaseDelivery.State/dbo/Tables/User.sql
+++ src/DatabaseDelivery.State/dbo/Tables/User.sql
@@ -4,6 +4,7 @@
     [LastName]     NVARCHAR (100) NOT NULL,
     [Email]        NVARCHAR (256) NOT NULL,
     [StatusID]     BIGINT         NOT NULL,
+    [BirthDate] DATETIME NULL, 
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC),
 	CONSTRAINT FK_User_UserStatus FOREIGN KEY ([StatusID]) REFERENCES UserStatus(UserStatusID)
 );
